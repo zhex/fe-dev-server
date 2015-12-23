7e461ae795682480a50023006ebcfa105e5f7d0d
ZheX <zhex81@gmail.com>
2015-12-24 01:27:39 +0800

    fix delay issue when template render error

--- handlers/view-handler.js
+++ handlers/view-handler.js
@@ -5,14 +5,16 @@ module.exports = function (req, res, next) {
 	var match = req._fds.match;
 	var config = req._fds.config;
 
-	setTimeout(function () {
-		res.render(match.file, req._fds.data, function (err, html) {
-			if (err) return next(err);
+	res.render(match.file, req._fds.data, function (err, html) {
+		if (err) return next(err);
 
-			if (config.livereload) {
-				html = lrScript.getInjectHtml(html);
-			}
+		if (config.livereload) {
+			html = lrScript.getInjectHtml(html);
+		}
+
+		setTimeout(function () {
 			res.send(html);
-		});
-	}, req._fds.delay);
+		}, req._fds.delay);
+	});
+
 };
